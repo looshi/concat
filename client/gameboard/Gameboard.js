8efb0ec4ff60d5dec31d826acc7c4dc635f39bd3
looshi <dluzius@edthena.com>
2015-10-10 17:42:21 -0700

    Added initial gameboard.

--- client/gameboard/Gameboard.js
+++ client/gameboard/Gameboard.js
@@ -1,25 +1,52 @@
-Gameboard.render = function(data, width, height){
+var BRUSH_SIZE = 4;
 
-  _.times(width){
-    _.times(height){
+Template.Gameboard.onCreated(function(){
+  this.isDrawing = new ReactiveVar(false);
+  this.rows = new ReactiveVar(null);
+  this.columns = new ReactiveVar(null);
+  var rows = [];
+  var columns = [];
+  for(var i=0;i<=80;i++){
+    rows.push(i);
+    columns[i] = [];
+    for(var j=0;j<=80;j++){
+      var cell={
+        row: i,
+        column: j,
+        color: '#ff00cc'
+      }
+      columns[i][j] = cell;
+   }
+  }
+  this.rows.set(rows);
+  this.columns.set(columns);
+  var self = this;
+  $('body').mouseup(function(){self.isDrawing.set(false);});
+  $('body').mouseleave(function(){self.isDrawing.set(false);});
+});
 
+Template.Gameboard.events({
+  'mousedown .gameboard-cell': function(){
+    Template.instance().isDrawing.set(true);
+  },
+  'mouseover .gameboard-cell': function(e){
+    if(Template.instance().isDrawing.get()){
+      var selector = [];
+      for(var i=this.row;i<this.row+BRUSH_SIZE;i++){
+        for(var j=this.column;j<this.column+BRUSH_SIZE;j++){
+          selector.push('#cell-'+i+'-'+j);
+        }
+      }
+      $(selector.join(', ')).css('background-color','#fff000');
     }
   }
-
-}
-
-Template.Gameboard.onRendered(function(){
-
-
-
 })
 
 Template.Gameboard.helpers({
   rows: function(){
-    return 80;
+    return Template.instance().rows.get();
   },
-  columns: function(){
-    return 80;
+  cell: function(row){
+    return Template.instance().columns.get()[row];
   }
-
 })
